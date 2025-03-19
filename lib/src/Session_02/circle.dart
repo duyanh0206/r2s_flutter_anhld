db484604e0c14c6aed33c6c065561e756fef103e
Le Duy Anh <anhleduy48@gmail.com>
2025-03-19 20:08:02 +0700

    fix circle

--- lib/src/Session_02/circle.dart
+++ lib/src/Session_02/circle.dart
@@ -1,6 +1,18 @@
 import 'dart:io';
 
 void main() {
+
+  void areaOfCircle() {
+    print('Enter the radius of the circle: ');
+    double radius = double.parse(stdin.readLineSync()!);
+    print('The area of the circle is ${3.14 * radius * radius}');
+  }
+  void perimeterOfCircle() {
+    print('Enter the radius of the circle: ');
+    double radius = double.parse(stdin.readLineSync()!);
+    print('The perimeter of the circle is ${2 * 3.14 * radius}');
+  }
+
   while (true) {
     print('1. Claclulate the area of a circle');
     print('2. Calculate the perimeter of a circle');
@@ -11,14 +23,10 @@ void main() {
 
     switch (choice) {
       case 1:
-        print('Enter the radius of the circle: ');
-        double radius = double.parse(stdin.readLineSync()!);
-        print('The area of the circle is ${3.14 * radius * radius}');
+        areaOfCircle();
         break;
       case 2:
-        print('Enter the radius of the circle: ');
-        double radius = double.parse(stdin.readLineSync()!);
-        print('The perimeter of the circle is ${2 * 3.14 * radius}');
+        perimeterOfCircle();
         break;
       case 3:
         exit(0);
